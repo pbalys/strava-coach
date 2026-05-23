6c0061c190cdc4301834b1b50a0440893a271c5e
Piotr Balys <pbalys@gmail.com>
2026-05-23 21:46:20 +0200

    remove app dir conflict

--- app/layout.js
+++
@@ -1,4 +0,0 @@
-export const metadata = { title: 'Strava Coach Dashboard' };
-export default function RootLayout({ children }) {
-  return <html lang="pl"><body style={{margin:0}}>{children}</body></html>;
-}
